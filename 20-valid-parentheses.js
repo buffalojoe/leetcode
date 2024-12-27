59837b9663e6d5d348418dfeecf942a466bf4b98
buffalojoe <42217874+buffalojoe@users.noreply.github.com>
2024-12-27 15:29:31 -0500

    Update 20-valid-parentheses.js
    Update to improve performance and memory usage

--- 20-valid-parentheses.js
+++ 20-valid-parentheses.js
@@ -1,8 +1,4 @@
 let isValid = function(s) {
-    // if an opening bracket is found, the next character must be an opening bracket or
-    //  a closing bracket of the same type
-    // if a closing bracket is found, the next character must be an opening bracket or
-    //  a closing bracket of an opening bracket that has already been used
 
     let openingBrackets = {
         '(': ')',
@@ -17,12 +13,10 @@ let isValid = function(s) {
     };
 
     unresolvedBrackets = [];
-    unresolvedBracketsAdded = 0;
 
     for (let char of s) {
         if (char in openingBrackets) {
             unresolvedBrackets.push(char);
-            unresolvedBracketsAdded += 1;
         } else if (char in closingBrackets && unresolvedBrackets.length == 0) {
             return false;
         } else {
@@ -34,13 +28,9 @@ let isValid = function(s) {
         }
     }
 
-    console.log(unresolvedBracketsAdded);
-
-    if (unresolvedBrackets.length == 0 && unresolvedBracketsAdded > 0) {
+    if (unresolvedBrackets.length == 0) {
         return true;
     } else {
         return false;
     }
 };
-
-console.log(isValid("()"))
